39fd77f355343baca5e7246ae08eadcbf314c0fb
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-12-21 17:17:32 -0800

    ugprade sdk (#6)
    * ugprade sdk
    
    * upgrade sdk
    
    * upgrade
    
    * check better
    
    * update tokenHook stuff
    
    Co-authored-by: Jonas Daniels <jonas.daniels@outlook.com>

--- src/shared/tokenHooks.ts
+++ src/shared/tokenHooks.ts
@@ -1,15 +1,23 @@
-import { CurrencyModule } from "@3rdweb/sdk";
+import { TokenModule } from "@3rdweb/sdk";
 import { AddressZero } from "@ethersproject/constants";
 import { BigNumber, BigNumberish } from "ethers";
 import { formatUnits, isAddress } from "ethers/lib/utils";
 import { useCallback, useEffect, useState } from "react";
 import { ChainIDToNativeSymbol } from "./commonRPCUrls";
 
+export const OtherAddressZero = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+
 export function isAddressZero(address: string): boolean {
-  return isAddress(address) && address === AddressZero;
+  const lowerCaseAddress = (address || "").toLowerCase();
+
+  return (
+    isAddress(lowerCaseAddress) &&
+    (lowerCaseAddress === AddressZero.toLowerCase() ||
+      lowerCaseAddress === OtherAddressZero.toLowerCase())
+  );
 }
 
-export function useTokenUnitConversion(tokenModule?: CurrencyModule) {
+export function useTokenUnitConversion(tokenModule?: TokenModule) {
   const format = useCallback(
     async (value: BigNumberish, chainId?: number) => {
       value = BigNumber.from(value);
@@ -18,11 +26,9 @@ export function useTokenUnitConversion(tokenModule?: CurrencyModule) {
         : "";
 
       // invalid token address
-      if (
-        !isAddress(tokenModule?.address || "") ||
-        isAddressZero(tokenModule?.address || "")
-      ) {
+      if (isAddressZero(tokenModule?.address || "")) {
         // default to 18 decimals
+
         return formatUnits(value.toString(), 18) + nativeCurrency;
       }
 
@@ -46,7 +52,7 @@ export function useTokenUnitConversion(tokenModule?: CurrencyModule) {
 
 export function useFormatedValue(
   value?: BigNumberish,
-  tokenModule?: CurrencyModule,
+  tokenModule?: TokenModule,
   chainId?: number,
 ) {
   const [formatted, setFormatted] = useState<string | undefined>();
@@ -58,5 +64,6 @@ export function useFormatedValue(
 
     format(value, chainId).then(setFormatted);
   }, [format, value, chainId]);
+
   return formatted;
 }
