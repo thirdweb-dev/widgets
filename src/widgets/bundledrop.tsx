adb4282341f74f2ac163f2ac3974b1eb68e1bc33
adam-maj <mr.adam.maj@gmail.com>
2022-01-29 13:04:47 -0500

    Update color scheme

--- src/widgets/bundledrop.tsx
+++ src/widgets/bundledrop.tsx
@@ -525,7 +525,7 @@ const DropWidget: React.FC<DropWidgetProps> = ({
       overflow="hidden"
       shadow="0px 1px 1px rgba(0,0,0,0.1)"
       border="1px solid"
-      borderColor="blackAlpha.10"
+      borderColor="blackAlpha.100"
       bg="whiteAlpha.100"
     >
       <Header
