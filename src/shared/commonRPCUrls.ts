a33b11dd2440b61b56293df32d522e24217289f8
adam-maj <mr.adam.maj@gmail.com>
2022-02-11 14:40:34 -0500

    Update mumbai rpc

--- src/shared/commonRPCUrls.ts
+++ src/shared/commonRPCUrls.ts
@@ -6,7 +6,7 @@ export const ChainIDToRPCMap: Record<number, string> = {
   137: "https://polygon-rpc.com",
   250: "https://rpc.ftm.tools",
   43114: "https://api.avax.network/ext/bc/C/rpc",
-  80001: "https://rpc-mumbai.maticvigil.com",
+  80001: "https://matic-mumbai.chainstacklabs.com",
 };
 
 export const ChainIDToNativeSymbol: Record<number, string> = {
