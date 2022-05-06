a388cb5e95a9caa9c4049b92afa58be0406c91c6
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-05-06 15:25:16 +0200

    Add fallback

--- src/widgets/bundledrop.tsx
+++ src/widgets/bundledrop.tsx
@@ -331,11 +331,11 @@ const ClaimButton: React.FC<ClaimPageProps> = ({
       {activeClaimCondition.data && (
         <Text size="label.md" color="green.800">
           {`${
-            Number(activeClaimCondition.data?.maxQuantity) -
-            Number(activeClaimCondition.data.availableSupply)
+            Number(activeClaimCondition.data?.maxQuantity || 0) -
+            Number(activeClaimCondition.data.availableSupply || 0)
           } ${
             activeClaimCondition.data?.maxQuantity !== "unlimited" &&
-            `/ ${activeClaimCondition.data?.maxQuantity}`
+            `/ ${activeClaimCondition.data?.maxQuantity || 0}`
           } claimed`}
         </Text>
       )}
